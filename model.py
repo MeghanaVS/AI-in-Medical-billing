62448af455912e0fd7328923abb4d814a78fbbf3
Meghana V S <meghanavs11@gmail.com>
2024-08-15 16:47:48 -0700

    Update model.py

--- model.py
+++ model.py
@@ -26,15 +26,13 @@ class PlmMultiLabelEncoder(Module):
         self._chunk_size: int = chunk_size
         self._chunk_num: int = chunk_num
 
-        # Layers and weights
-        # `V` matrix in paper
         self._label_attn_linear_weights_0: FloatTensor = Parameter(
             torch.rand(
                 size=(first_attn_hidden_dim, lm_embd_dim), dtype=torch.float32
             ), requires_grad=True
         )
         self.register_parameter("label_attn_linear_weights_0", self._label_attn_linear_weights_0)
-        # `W` matric in paper
+
         self._label_attn_linear_weights_1: FloatTensor = Parameter(
             torch.rand(
                 size=(label_num, first_attn_hidden_dim), dtype=torch.float32
@@ -62,46 +60,32 @@ class PlmMultiLabelEncoder(Module):
         assert(token_ids.shape[1] == self._chunk_num)
         assert(token_ids.shape[2] == self._chunk_size)
         assert(token_ids.shape == attn_masks.shape)
-        
-        # Unnest chunks, which means after that we get a batch of chunks' token IDs, 
-        # several chunks belongs to same text.
+
         unnested_token_ids: LongTensor = token_ids.view(-1, self._chunk_size)
         unnested_attn_masks: LongTensor = attn_masks.view(-1, self._chunk_size)
 
-        # Index 0 is '[cls]' token's embedding, dimension: 
-        # (batch-size * chunk-num, LM-hidden-dimension)
         unnested_embeddings: FloatTensor = self._lm(
             input_ids=unnested_token_ids, attention_mask=unnested_attn_masks, 
             output_hidden_states=True
         ).hidden_states[-1][:, -1, :]
-         
-        # Put unnested chunks embeddings into chunk tensors, the dimension is:
-        # (batch-size, chunk-number, LM-hidden-dimension)
+
         chunk_embeddings: FloatTensor = unnested_embeddings.view(
             -1, self._chunk_num, self._lm_embd_dim
         )
-        # As in paper, chunk embedding should be the column vectors in chunk tensor, 
-        # after following operation, the dimension becomens to:
-        # (batch-size, LM-hidden-dimension, chunk-number)
+
         chunk_embeddings = chunk_embeddings.swapaxes(2, 1) 
-        
-        # First linear transformation to label attention, AKA `Z` in paper, 
-        # the dimension is: (batch-size, `first_attn_hidden_dim`, chunk-num)
+  
         linear_trans_0: FloatTensor = self._label_attn_linear_weights_0.matmul(
             chunk_embeddings
         )
         linear_trans_0 = torch.tanh(linear_trans_0)
-        # Second linear transformation to label attention, AKA `A` in paper, 
-        # the dimension is (batch-size, label-num, chunk-num)
+
         linear_trans_1: FloatTensor = self._label_attn_linear_weights_1.matmul(
             linear_trans_0
         )
         linear_trans_1 = self._label_attn_linear_1_act(linear_trans_1)
 
-        # Weighted chunked embeddings, corresponding to matrix `D` in paper.
-        # Each column of `D` represent a document embedding for specific 
-        # label (which is a weighted sum of each chunk embedding).
-        # The dimension is: (batch-size, LM-hidden-dimension, label-num)
+        
         weighted_chunk_embeddings: FloatTensor = chunk_embeddings.matmul(
             # dim: (batch-size, chunk-num, label-num)
             linear_trans_1.transpose(2, 1)
